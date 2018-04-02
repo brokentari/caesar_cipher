953eec5badd4276259c3253e3ab885dc0759abcc
saul <saul.reyna90@gmail.com>
2018-04-02 16:45:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,140 +7,135 @@ let ascii_array = [];
 let modified_ascii_array = [];
 let modified_input_text_char = [];
 
+
 function clearInput() {
     document.getElementById("encrypt_input").value = "";
     ascii_array.length = 0;
     modified_ascii_array.length = 0;
     modified_input_text_char.length = 0;
+    document.getElementById("modified_text").innerHTML = "";
 }
 
-function convertToASCII() {
+
+function convertToString(characterASCII) {
     input_text = document.getElementById("encrypt_input").value;
     for (let i = 0; i < input_text.length; i++) {
         ascii_array.push(input_text.charCodeAt(i));
     }
+    return String.fromCharCode(characterASCII);
 }
 
-function convertToString() {
-    document.getElementById("modified_text").innerHTML = "";
-    for (let i = 0; i < modified_ascii_array.length; i++) {
-        modified_input_text_char.push(String.fromCharCode(modified_ascii_array[i]));
-    }
-    document.getElementById("modified_text").innerHTML = modified_input_text_char.join("");
-}
-
-/*
-function standardProcedure(input_char, modified_input_char) {
+function encrypt(characterASCII) {
+    let input_char = characterASCII;
+    let modified_input_char = characterASCII - 5;
     if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
-        modified_ascii_array.push(input_char);
-        console.log(input_char + " was added to the array");
+        return input_char;
     }
 
     else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
-        modified_ascii_array.push(modified_input_char);
-        console.log(modified_input_char + " was added to the array");
+        return modified_input_char;
     }
-}
-*/
 
-function encrypt() {
-    convertToASCII();
-
-    for (let k = 0; k < ascii_array.length; k++) {
-        let input_char = ascii_array[k];
-        let modified_input_char = ascii_array[k] - 5;
-
-        if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
-            modified_ascii_array.push(input_char);
-            console.log(input_char + " was added to the array");
-        }
-
-        else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
-            modified_ascii_array.push(modified_input_char);
-            console.log(modified_input_char + " was added to the array");
-        }
-
-        else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (65 <= modified_input_char && modified_input_char <= 69)) {
-            let single_char = input_char;
-            let count = 0;
-            if (modified_input_char < LOWER_LIMIT_LOWERCASE) {
-                while (single_char - 1 >= LOWER_LIMIT_LOWERCASE) {
-                    single_char = single_char - 1;
-                    count = count + 1;
-                }
-                modified_ascii_array.push((UPPER_LIMIT_CAPITAL + 1) - (5 - count));
+    else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (65 <= modified_input_char && modified_input_char <= 69)) {
+        let single_char = input_char;
+        let count = 0;
+        if (modified_input_char < LOWER_LIMIT_LOWERCASE) {
+            while (single_char - 1 >= LOWER_LIMIT_LOWERCASE) {
+                single_char = single_char - 1;
+                count = count + 1;
             }
-            else if (modified_input_char < LOWER_LIMIT_CAPITAL) {
-                while (single_char - 1 >= LOWER_LIMIT_CAPITAL) {
-                    single_char = single_char - 1;
-                    count = count + 1;
-                    
-                }
-                modified_ascii_array.push((UPPER_LIMIT_LOWERCASE + 1) - (5 - count));
+            return ((UPPER_LIMIT_CAPITAL + 1) - (5 - count));
+        }
+        else if (modified_input_char < LOWER_LIMIT_CAPITAL) {
+            while (single_char - 1 >= LOWER_LIMIT_CAPITAL) {
+                single_char = single_char - 1;
+                count = count + 1;
+
             }
+            return ((UPPER_LIMIT_LOWERCASE + 1) - (5 - count));
         }
     }
-    convertToString();
+
 }
 
-function decrypt() {
-    convertToASCII();
-
-    for (let k = 0; k < ascii_array.length; k++) {
-        let input_char = ascii_array[k];
-        let modified_input_char = ascii_array[k] + 5;
+function decrypt(characterASCII) {
+    let input_char = characterASCII
+    let modified_input_char = characterASCII + 5;
         
-        if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
-            modified_ascii_array.push(input_char);
-            console.log(input_char + " was added to the array");
-        }
+    if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
+        return input_char;
+    }
 
-        else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
-            modified_ascii_array.push(modified_input_char);
-            console.log(modified_input_char + " was added to the array");
-        }
+    else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
+        return modified_input_char;
+    }
 
-        else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (117 <= modified_input_char && modified_input_char <= 122)) {
-            let single_char = input_char;
-            let count = 0;
-            if (modified_input_char > UPPER_LIMIT_CAPITAL) {
-                while (single_char + 1 <= UPPER_LIMIT_CAPITAL) {
-                    single_char = single_char + 1;
-                    count = count + 1;
-                }
-                modified_ascii_array.push((LOWER_LIMIT_LOWERCASE - 1) + (5 - count));
+    else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (117 <= modified_input_char && modified_input_char <= 122)) {
+        let single_char = input_char;
+        let count = 0;
+        if (modified_input_char > UPPER_LIMIT_CAPITAL) {
+            while (single_char + 1 <= UPPER_LIMIT_CAPITAL) {
+                single_char = single_char + 1;
+                count = count + 1;
             }
-            else if (modified_input_char > UPPER_LIMIT_LOWERCASE) {
-                while (single_char + 1 >= LOWER_LIMIT_CAPITAL) {
-                    single_char = single_char - 1;
-                    count = count + 1;
-                    
-                }
-                modified_ascii_array.push((LOWER_LIMIT_CAPITAL - 1) + (5 - count));
+            return ((LOWER_LIMIT_LOWERCASE - 1) + (5 - count));
+        }
+        else if (modified_input_char > UPPER_LIMIT_LOWERCASE) {
+            while (single_char + 1 >= LOWER_LIMIT_CAPITAL) {
+                single_char = single_char - 1;
+                count = count + 1;
+
             }
+            return ((LOWER_LIMIT_CAPITAL - 1) + (5 - count));
         }
     }
-    convertToString();
 }
 
-window.onload = function() {
-    document.getElementById("encrypt_input").addEventListener("click", clearInput);
+function encryptAsYouType(e) {
+    const keycode = e.which;
+    console.log(keycode);
 
-    document.getElementById("encrypt_input").addEventListener("keypress", function (e) {
-        if (e.which === 13) {
-            encrypt();
-            clearInput();
+    document.getElementById("modified_text").innerHTML = document.getElementById("modified_text").innerHTML + convertToString(encrypt(keycode));
+}
+
+function decryptAsYouType(e) {
+    const keycode = e.which;
+    document.getElementById("modified_text").innerHTML = document.getElementById("modified_text").innerHTML + convertToString(decrypt(keycode));
+}
+
+window.onload = function () {
+    const input_bar = document.getElementById("encrypt_input");
+    const encrypt_button = document.getElementById("encryption");
+    const decrypt_button = document.getElementById("decryption");
+    
+    input_bar.addEventListener("keydown", function (e) {
+        const keycode = e.which;
+        if (keycode === 8) {
+            console.log(keycode);
+            document.getElementById("modified_text").innerHTML = document.getElementById("modified_text").innerHTML.substring(0, document.getElementById("modified_text").innerHTML.length - 1);
         }
     });
+   input_bar.addEventListener("click", clearInput);
 
-    document.getElementById("encryption").addEventListener("click", function() {
-        encrypt();
+   document.getElementById("encryption").addEventListener("click", function () {
         clearInput();
+        document.getElementById("decryption").style.color = "#ffffff";
+        document.getElementById("decryption").style.backgroundColor = "#000000";
+        document.getElementById("decryption").style.borderColor = "#000000";
+        encrypt_button.style.color = "#000000";
+        encrypt_button.style.backgroundColor = "#ffffff";
+        encrypt_button.style.borderColor = "#ffffff";
+        input_bar.addEventListener("keypress", encryptAsYouType);
     });
 
-    document.getElementById("decryption").addEventListener("click", function() {
-        decrypt();
+    document.getElementById("decryption").addEventListener("click", function () {
         clearInput();
+        encrypt_button.style.color = "#ffffff";
+        encrypt_button.style.backgroundColor = "#000000";
+        encrypt_button.style.borderColor = "#000000";
+        decrypt_button.style.color = "#000000";
+        decrypt_button.style.backgroundColor = "#ffffff";
+        decrypt_button.style.borderColor = "#ffffff";
+        input_bar.addEventListener("keypress", decryptAsYouType);
     });
-
 };
