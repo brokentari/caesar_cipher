898bfb4bb64b0debf9863178d24119e27c7e933d
saul <saul.reyna90@gmail.com>
2018-04-02 21:37:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const LOWER_LIMIT_CAPITAL = 65;
 const UPPER_LIMIT_LOWERCASE = 122;
 const LOWER_LIMIT_LOWERCASE = 97;
 let input_text;
+let ascii_array = [];
+let modified_ascii_array = [];
+let modified_input_text_char = [];
 
 
 function clearInput() {
@@ -27,7 +30,7 @@ function encrypt(characterASCII) {
         return modified_input_char;
     }
 
-    else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (65 <= modified_input_char && modified_input_char <= 69)) {
+    else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (65 < modified_input_char && modified_input_char <= 69)) {
         let single_char = input_char;
         let count = 0;
         if (modified_input_char < LOWER_LIMIT_LOWERCASE) {
@@ -46,7 +49,6 @@ function encrypt(characterASCII) {
             return ((UPPER_LIMIT_LOWERCASE + 1) - (5 - count));
         }
     }
-
 }
 
 function decrypt(characterASCII) {
@@ -78,14 +80,93 @@ function decrypt(characterASCII) {
             }
             return ((LOWER_LIMIT_CAPITAL - 1) + (5 - count));
         }
-        console.log(count);
     }
+}
+
+function encryptPasted(pastedText) {
+    for (let i = 0; i < pastedText.length; i++) {
+        ascii_array.push(pastedText.charCodeAt(i));
+    }
+
+    for (let k = 0; k < ascii_array.length; k++) {
+        let input_char = ascii_array[k];
+        let modified_input_char = input_char - 5;
+        if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
+            modified_ascii_array.push(input_char);
+        }
+        else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
+            modified_ascii_array.push(modified_input_char);
+        }
+        else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (65 < modified_input_char && modified_input_char <= 69)) {
+            let single_char = input_char;
+            let count = 0;
+            if (modified_input_char < LOWER_LIMIT_LOWERCASE) {
+                while (single_char - 1 >= LOWER_LIMIT_LOWERCASE) {
+                    single_char = single_char - 1;
+                    count = count + 1;
+                }
+                modified_ascii_array.push((UPPER_LIMIT_CAPITAL + 1) - (5 - count));
+            }
+            else if (modified_input_char < LOWER_LIMIT_CAPITAL) {
+                while (single_char - 1 >= LOWER_LIMIT_CAPITAL) {
+                    single_char = single_char - 1;
+                    count = count + 1;
+
+                }
+                modified_ascii_array.push((UPPER_LIMIT_LOWERCASE + 1) - (5 - count));
+            }
+        }
+    }
+
+    for (let j = 0; j < modified_ascii_array.length; j++) {
+        modified_input_text_char.push(String.fromCharCode(modified_ascii_array[j]));
+    }
+    return modified_input_text_char.join("");
+}
+
+function decryptPasted(pastedText) {
+    for (let i = 0; i < pastedText.length; i++) {
+        ascii_array.push(pastedText.charCodeAt(i));
+    }
+
+    for (let k = 0; k < pastedText.length; k++) {
+        let input_char = ascii_array[k];
+        let modified_input_char = input_char + 5;
+
+        if (input_char < LOWER_LIMIT_CAPITAL || input_char > UPPER_LIMIT_LOWERCASE || (UPPER_LIMIT_CAPITAL < input_char && input_char < LOWER_LIMIT_LOWERCASE)) {
+            modified_input_char.push(input_char);
+        }
+
+        else if ((LOWER_LIMIT_CAPITAL <= modified_input_char && modified_input_char <= UPPER_LIMIT_CAPITAL) || (LOWER_LIMIT_LOWERCASE <= modified_input_char && modified_input_char <= UPPER_LIMIT_LOWERCASE)) {
+            modified_input_char.push(modified_input_char);
+        }
+        else if ((UPPER_LIMIT_CAPITAL < modified_input_char && modified_input_char < LOWER_LIMIT_LOWERCASE) || (modified_input_char > UPPER_LIMIT_LOWERCASE)) {
+            let single_char = input_char;
+            let count = 0;
+            if (modified_input_char > UPPER_LIMIT_CAPITAL && modified_input_char < LOWER_LIMIT_LOWERCASE) {
+                while (single_char + 1 <= UPPER_LIMIT_CAPITAL) {
+                    single_char = single_char + 1;
+                    count = count + 1;
+                }
+                modified_input_char.push((LOWER_LIMIT_LOWERCASE - 1) + (5 - count));
+            }
+            else if (modified_input_char > UPPER_LIMIT_LOWERCASE) {
+                while (single_char + 1 <= UPPER_LIMIT_LOWERCASE) {
+                    single_char = single_char + 1;
+                    count = count + 1;
+                }
+                modified_input_char((LOWER_LIMIT_CAPITAL - 1) + (5 - count));
+            }
+        }
+    }
+    for (let j = 0; j < modified_ascii_array.length; j++) {
+        modified_input_text_char.push(String.fromCharCode(modified_ascii_array[j]));
+    }
+    return modified_input_text_char.join("");
 }
 
 function encryptAsYouType(e) {
     const keyCode = e.which;
-    console.log(keyCode);
-
     document.getElementById("modified_text").innerHTML = document.getElementById("modified_text").innerHTML + convertToString(encrypt(keyCode));
 }
 
@@ -102,11 +183,15 @@ window.onload = function () {
     input_bar.addEventListener("keydown", function (e) {
         const keyCode = e.which;
         if (keyCode === 8) {
-            console.log(keyCode);
             document.getElementById("modified_text").innerHTML = document.getElementById("modified_text").innerHTML.substring(0, document.getElementById("modified_text").innerHTML.length - 1);
         }
     });
     input_bar.addEventListener("click", clearInput);
+    input_bar.addEventListener("paste", function(e) {
+        const pasted = e.clipboardData.getData('text');
+        document.getElementById("modified_text").innerHTML = decryptPasted(pasted);
+    });
+
     input_bar.addEventListener("keypress", encryptAsYouType);
     document.getElementById("encryption").addEventListener("click", function () {
         clearInput();
@@ -117,6 +202,10 @@ window.onload = function () {
         encrypt_button.style.color = "#000000";
         encrypt_button.style.backgroundColor = "#ffffff";
         encrypt_button.style.borderColor = "#ffffff";
+        input_bar.addEventListener("paste", function(e) {
+            const pasted = e.clipboardData.getData('text');
+            document.getElementById("modified_text").innerHTML = encryptPasted(pasted);
+        });
         input_bar.addEventListener("keypress", encryptAsYouType);
     });
 
@@ -129,6 +218,10 @@ window.onload = function () {
         decrypt_button.style.color = "#000000";
         decrypt_button.style.backgroundColor = "#ffffff";
         decrypt_button.style.borderColor = "#ffffff";
+        input_bar.addEventListener("paste", function(e) {
+            const pasted = e.clipboardData.getData('text');
+            document.getElementById("modified_text").innerHTML = decryptPasted(pasted);
+        });
         input_bar.addEventListener("keypress", decryptAsYouType);
     });
 };
